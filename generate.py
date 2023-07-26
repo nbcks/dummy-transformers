172183105a42358c2f9f406edc717b3bde2d9248
nicholas.buckner <nicholas.buckner@yahoo.com>
2023-07-26 21:43:49 +0100

    it runs for ever

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 from typing import List
+from random import Random
 
 class Node:
 	pass
@@ -28,8 +29,40 @@ Sentence = Composite()
 Sentence.append_rule([ Nouns, Verbs, Nouns ]).append_rule([ Sentence, Joiners, Sentence ])
 
 def generate_random_sentences(num : int, seed : int, ruleset : Node) -> str:
-	pass
+	strs = [];
+	rand = Random()
+	rand.seed(0)
+
+		
+	while num >= 0:
+		# do a random depth first search
+		cur_stack = [ruleset]
+		cur_str = ""
+		while len(cur_stack) != 0:
+			cur_node = cur_stack.pop()
+			if isinstance(cur_node, Empty):
+				pass
+			elif isinstance(cur_node, Tokens):
+				rand_index = rand.randrange(0, len(cur_node.Tokens))
+				rand_str = cur_node.Tokens[rand_index]
+				cur_str += rand_str
+			elif isinstance(cur_node, Composite):
+				rand_index = rand.randrange(0, len(cur_node.Rules))
+				rand_rule = cur_node.Rules[rand_index]# todo!
+				for node in reversed(rand_rule): # not 100% sure this is right
+					cur_stack.append(node)
+			else:
+				raise "Impossible"
+			
+
+		strs += cur_str
+		num += 1
 
 # quite hard skipping for now
 def is_valid(sentence : str, ruleset : Node):
 	pass
+
+strs = generate_random_sentences(10, 0, Sentence)
+print("generating stuff")
+for str in strs:
+	print(str)
